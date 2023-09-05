48ab5b7f4cbdcf9845387b6a5c2ab8c8d1e586bd
Nirav Shah <niravbinga33@gmail.com>
2023-09-05 09:53:35 +0530

    Check code formatting

---
+++ src/main/java/co/pseudocode/javacodeformatter/AutoFormatter.java
@@ -0,0 +1,25 @@
+package co.pseudocode.javacodeformatter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class AutoFormatter {
+     private String name;private List<String> values;
+
+  public String getName() {
+    return name;
+  }  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<String> getValues() {
+    return values;
+  }
+
+  
+  /* java code with comments*/ 
+  public void setValues(List<String> values) {
+    this.values = values;
+  }
+}
--- src/test/java/co/pseudocode/javacodeformatter/JavaCodeFormatterApplicationTests.java
+++ src/test/java/co/pseudocode/javacodeformatter/JavaCodeFormatterApplicationTests.java
@@ -1,13 +1,12 @@
 package co.pseudocode.javacodeformatter;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavaCodeFormatterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
