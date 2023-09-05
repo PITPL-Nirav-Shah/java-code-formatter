fe5c1388265d4c6be920f6012f368900ced3a272
Nirav Shah <niravbinga33@gmail.com>
2023-09-05 09:02:19 +0530

    Initial Commit

---
+++ src/main/java/co/pseudocode/javacodeformatter/JavaCodeFormatterApplication.java
@@ -0,0 +1,13 @@
+package co.pseudocode.javacodeformatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeFormatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeFormatterApplication.class, args);
+	}
+
+}
